0330e8dd3ffcd9c66eb2f79ca3c1d8f24a069a10
guilh <guilhermesoares278@gmail.com>
2023-01-23 21:41:53 -0300

    lista 11 e 12

--- Lista 11 - quickSort/A - quickSort.c
+++ Lista 11 - quickSort/A - quickSort.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 typedef int item;
 #define less(a,b) (a<b)
+#define cmpexch(A,B) {if(less(B,A)) exch(A,B)}
 #define exch(a,b) {item tt=a; \
                    a = b; \
                    b = tt; }
@@ -8,7 +9,7 @@ typedef int item;
 int separa(item *v, int l, int r){
     item p = v[r];
     int i=l-1, j=r;
-    for(; ;){
+    for( ; ; ){
         while (less(v[++i], p));
         while (less(p, v[--j])) if (j==l) break;
         if (i>=j) break;
@@ -19,24 +20,31 @@ int separa(item *v, int l, int r){
 }
 
 void quickSort(item *v, int l, int r){
-    if (l>=r) return;
+    if(l>=r) return;
+
+    cmpexch(v[(l+r)/2],v[r]);
+    cmpexch(v[l],v[(l+r)/2]);
+    cmpexch(v[r],v[(l+r)/2]);
+
     int j = separa(v, l, r);
     quickSort(v, l, j-1);
     quickSort(v, j+1, r);
+
 }
 
 int main(){
-    int vetor[1000] = {7, 3, 5, 2, 66, 3, 24, 6, 235, 5, 2, 3, 7, 8, 9, 7,5 , 4};
-    int i=0;
+    int n;
+    scanf("%d", &n);
+    int vetor[n];
 
-    while (scanf("%d", &vetor[i]) != EOF){
-        i++;
+    for(int i=0; i<n; i++){
+        scanf("%d", &vetor[i]);
     }
 
-    quickSort(vetor, 0, i-1);
+    quickSort(vetor, 0, n-1);
     
-    for (int j=0; j<i-1; j++){
+    for (int j=0; j<n-1; j++){
         printf("%d ", vetor[j]);
     }
-    printf("%d\n", vetor[i-1]);
+    printf("%d\n", vetor[n-1]);
 }
